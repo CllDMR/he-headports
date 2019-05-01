f607bf68812a59821ad9960d1d1e95a47c520fd3
CllDMR <celal.demirx@gmail.com>
2019-05-01 09:27:32 +0300

    asd

--- app.js
+++ app.js
@@ -41,21 +41,11 @@ app.use(upload.none());
 // parse application/json
 app.use(bodyParser.json());
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-// Make our db accessible to our router
-app.use(function(req, res, next) {
-  req.db = db;
-  next();
-});
 
 app.use("/", require("./routes"));
 
